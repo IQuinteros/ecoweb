b3579f0307f1101480086338944a733b426b532a
luisFelipeMG <84549000+luisFelipeMG@users.noreply.github.com>
2021-05-31 21:35:52 -0400

    Create register.php

---
+++ register.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+        <form>
+        <label for="Pname"> public name:</label>
+        <input type="text" id="Pname" name="Pname"><br><br>
+        <label for="description">description:</label>
+        <input type="text" id="description" name="description"><br><br>
+        <label for="email">email:</label>
+        <input type="text" id="email" name="email"><br><br>
+        <label for="location">email:</label>
+        <input type="text" id="location" name="location"><br><br>
+        <label for="passwords">passwords:</label>
+        <input type="text" id="passwords" name="passwords"><br><br>
+        <label for="rut">rut:</label>
+        <input type="text" id="rut" name="rut">
+        <label for="rut_cd">rut cd:</label>
+        <input type="text" id="rut_cd" name="rut_cd"><br><br>
+        <section name='district' id='district'></section>
+        <input type="submit" value="Submit">
+      </form>
+</html>
