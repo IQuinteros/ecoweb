c7f6984cc2ed5145356daf0d127bfc6afb5069f2
IQuinteros <i.quinteros@hotmail.com>
2021-06-07 00:45:22 -0400

    Clean html

--- api/requests/profile/profile_select.php
+++ api/requests/profile/profile_select.php
@@ -3,19 +3,15 @@ require_once('../base_request.php');
 require_once('../../query/profile.php');
 $profiles = new profile();
 // Assign result to variable
-$result = $profiles->select_profile();?>
-<html>
-    <head></head>
-    <body>
-        <?php
-        if(is_null($profiles)){
-            send_response(false, null, "Profile no encontrado"); 
-        }
-        else{
-            // Return result
-            send_response(true, $result);            
-        }
-        ?>
-    </body>
-</html>
+$result = $profiles->select_profile();
+
+if(is_null($profiles)){
+    send_response(false, null, "Profile no encontrado"); 
+}
+else{
+    // Return result
+    send_response(true, $result);            
+}
+
+
 
