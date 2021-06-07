2c49727ab7c064cec9b8055bddbbfa3d04b00248
IQuinteros <i.quinteros@hotmail.com>
2021-06-06 23:04:36 -0400

    Adding connection hosting

--- api/Connection.php
+++ api/Connection.php
@@ -66,6 +66,22 @@ class Connection{
         die();
     	}
 	}
+	public function connection_hosting(){
+		$dsn = 'mysql:host=localhost;port=3306;dbname=id16988549_ecomercio';
+		$username = 'id16988549_admin';
+		$password = 'ruJxus-kidky3-puhbox';
+		$options = array(
+		    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'",
+		    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+		    PDO::MYSQL_ATTR_FOUND_ROWS => true
+		);
+    	try {
+        $this->pdo = new PDO($dsn, $username, $password, $options);
+    	} catch (PDOException $th) {
+        echo $th->getMessage();
+        die();
+    	}
+	}
 	public function debug_to_console( $data ) {
 		$output = $data;
 		if ( is_array( $output ) )
