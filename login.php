07df8d5e09344a1bdc26998a3ec7fbb37c8cfa65
Luis Felipe <luis.montero08@inacapmail.cl>
2021-06-09 19:14:47 -0400

    cambio login

--- login.php
+++ login.php
@@ -7,6 +7,22 @@
     <title>login</title>
 </head>
 <body>
-    
+<?php
+ini_set('display_errors', 1);
+error_reporting(~0);
+
+//require_once('api/query/store.php');
+//$storeConnection = new Store();
+
+require_once('include.php');
+
+ ?>
+<form id="login_store">
+        <label for="Pname"> Nombre o Correo</label>
+        <input type="text" id="PNoC" name="PNoC"><br><br>
+        <label for="passwords">contraseña </label>
+        <input type="text" id="passwords" name="passwords"><br><br>
+        <button onclick="sendData(login_store)">login</button>
+      </form>
 </body>
 </html>
