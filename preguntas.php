7961659666a1bde193e5aef634755eab19a95ae6
Luis Felipe <luis.montero08@inacapmail.cl>
2021-06-11 19:43:58 -0400

    <navegar>

--- preguntas.php
+++ preguntas.php
@@ -7,6 +7,15 @@
     <title>Document</title>
 </head>
 <body>
-    
+<ul></ul>
+<li><a href="home.php">home</a></li>
+<li><a href="pedidos.php">pedidos</a><li>
+<li><a href="Chats.php">Chats</a><li>
+<li><a href="preguntas.php">preguntas</a><li>
+<li><a href="valoraciones.php">valoraciones</a><li>
+<li><a href="inventario.php">inventario</a><li>
+<li><a href="reportes.php">reportes</a><li>
+<li><a href="perfil.php">perfil</a><li>
+</ul> 
 </body>
 </html>
