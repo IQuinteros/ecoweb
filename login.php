ab6527c75434955ec43613b9c265bd264c83f035
Luis Felipe <luis.montero08@inacapmail.cl>
2021-06-16 18:30:53 -0400

    cambios en el login

--- login.php
+++ login.php
@@ -1,10 +1,15 @@
 <?php 
- if(isset($_POST['submit'])){     
+ if(isset($_POST['submit'])){        
      $email =$_POST['email'];
      $passwords=$_POST['passwords'];
      require_once __DIR__.('/api/query/store.php');
      $storeConnection = new Store();
-     //$login_store = $storeConnection->login($correo, $passwords);
+     $login_store = $storeConnection->login($email, $passwords);
+     session_start();
+    // Hiciste el login 
+     $store;
+
+     $_SESSION["id"] = $store->id;
  }
 ?>
 <!DOCTYPE html>
@@ -17,30 +22,30 @@
 </head>
 <body>
 <?php
+ $_SESSION["id"] =null;
 ini_set('display_errors', 1);
 error_reporting(~0);
 require_once('include.php');
-
-session_start();
-// Hiciste el login 
-$store;
-
-$_SESSION["id"] = $store->id;
-
  ?>
+ <script>
+ if( $_SESSION["id"] == null){
+ var page="login.php";
+ }else{if($_SESSION["id"] != null
+ ){
+    var page="home.php"
+ }}
+ 
+ </script>
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post" id="login_store">
         <label for="email">Correo</label>
         <input type="text" id="email" name="email"><br><br>
         <label for="passwords">contraseña </label>
         <input type="text" id="passwords" name="passwords"><br><br>
-        <button onclick="sendData(login_store)" value="login" name="submit"
-        <?php         
-        
-   //         if(isset($store)){
-      //      window.open('home.php')
-       // }
+        <a href="<script type='text/javascript'> document.write(page); </script>" value="ir"> <button onclick="sendData(login_store)" value="login" name="submit"
+        <?php             
         ?>
-       >login</button>
+       >login</button></a>
       </form>
+
 </body>
 </html>
