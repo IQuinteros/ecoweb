1ac44dfcd8776e0344161990e54929331eabb2c2
IQuinteros <i.quinteros@hotmail.com>
2021-06-20 16:37:25 -0400

    Profile api fixes

--- api/requests/profile/profile_insert.php
+++ api/requests/profile/profile_insert.php
@@ -4,6 +4,6 @@ require_once('../../query/profile.php');
 
 $profiles = new Profile();
 $result=$profiles->insert_profile($data->name, $data->last_name, $data->email, $data->contact_number, $data->birthday, 
-$data->terms_checked, $data->location, $data->passwords, $data->rut, $data->rut_cd, $data->district, $data->user_id);
+$data->terms_checked, $data->location, $data->passwords, $data->rut, $data->rut_cd, $data->district_id, $data->user_id);
 
 send_response(isset($result), $result); 
