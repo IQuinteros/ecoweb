db6ba016dcc6a0b35f881cbb7cd1993f90a81557
IQuinteros <i.quinteros@hotmail.com>
2021-06-22 22:53:48 -0400

    Chart styling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/* APP BAR */
 let appBarOpen = false;
 
 function toggleMenu(){
@@ -8,3 +8,45 @@ function toggleMenu(){
         : appbar.classList.add('open')
     appBarOpen = !appBarOpen;
 }
+
+/* CHART */
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'line',
+    data,
+    options: {
+        responsive: true,
+        maintainAspectRatio: true
+    }
+};
+
+let myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+
+myChart.width = parent.offsetWidth;
+myChart.height = parent.offsetWidth;
+
+window.addEventListener('resize', () => {
+    myChart.width = 10
+    myChart.height = 10
+});
